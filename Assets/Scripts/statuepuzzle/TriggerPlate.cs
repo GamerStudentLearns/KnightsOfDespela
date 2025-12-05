533f830cbd27f2bf3ae1976128121642abc9b0ea
GamerStudentLearns <767475@newcollege.ac.uk>
2025-12-05 14:21:26 +0000

    aaaazaaaaaa

---
+++ Assets/Scripts/statuepuzzle/TriggerPlate.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+
+public class TriggerPlate : MonoBehaviour
+
+{
+
+    [SerializeField] private TriggerActivator activator;
+
+    [SerializeField] private string requiredTag = "PuzzleObject";
+
+    private bool isOccupied = false;
+
+    private void OnTriggerEnter(Collider other)
+
+    {
+
+        if (isOccupied) return;
+
+        if (!other.CompareTag(requiredTag)) return;
+
+        if (activator == null)
+
+        {
+
+            Debug.LogError("❌ TriggerPlate on " + gameObject.name + " has NO Activator assigned!");
+
+            return;
+
+        }
+
+        isOccupied = true;
+
+        activator.AddTrigger();
+
+    }
+
+    private void OnTriggerExit(Collider other)
+
+    {
+
+        if (!isOccupied) return;
+
+        if (!other.CompareTag(requiredTag)) return;
+
+        isOccupied = false;
+
+        activator.RemoveTrigger();
+
+    }
+
+}
